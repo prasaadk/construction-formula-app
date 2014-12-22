7c0be85b6dc82e2eebf6e88cb3e11307d8d3176f
Prasad Kulkarni <prasad.fromgroup@gmail.com>
2014-12-22 00:54:34 +0000

    Finished find quantities page.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,48 +4,108 @@ angular.module('starter.controllers', [])
 
 .controller('AboutCtrl', function($scope) {})
 
-.controller('FindRatioCtrl', function($scope) {
-  $scope.validationError = {};
-  $scope.ratioData = {};
-  $scope.postData = {grade: "20", sandProportion: "50"};
-  $scope.gradeRange = [6,7.5,10]
-  for (i = 15;i<=80;i+=5) {
-      $scope.gradeRange.push(i);
+.controller('FindRatioCtrl', ['$rootScope', '$scope', function($rootScope, $scope) {
+  $rootScope.postData = {grade: "20", sandProportion: "50"};
+
+  $rootScope.crushedSandProportion = function () {
+      return 100 - parseInt($rootScope.postData.sandProportion);
   }
 
-  $scope.crushedSandProportion = function () {
-      return 100 - parseInt($scope.postData.sandProportion);
+  $rootScope.riverSandProportion = function () {
+      return parseInt($rootScope.postData.sandProportion);
   }
 
-  $scope.riverSandProportion = function () {
-      return parseInt($scope.postData.sandProportion);
-  }
-
-  $scope.ratio = function() {
+  $rootScope.ratio = function() {
       var constant = 30;
-      var grade = parseInt($scope.postData.grade);
+      var grade = parseInt($rootScope.postData.grade);
 
       var cement = 1;
       var sand = constant / grade;
       var aggregate = 2 * constant / grade;
-      var ratio = {cement: cement, sand: sand.toFixed(2), aggregate: aggregate.toFixed(2)};
-      $scope.ratioData = ratio;
+      var cs = sand*$rootScope.crushedSandProportion()/100;
+      var rs = sand*$rootScope.riverSandProportion()/100;
+      var ratio = {cement: cement, 
+                   sand: parseFloat(sand.toFixed(2)), 
+                   cs: parseFloat(cs.toFixed(2)), 
+                   rs: parseFloat(rs.toFixed(2)),
+                   aggregate: parseFloat(aggregate.toFixed(2))
+                  };
       return ratio;
   };
-})
+
+}])
 
 
-.controller('FindQuantityCtrl', function($scope) {
+.controller('FindQuantityCtrl', ['$rootScope', '$scope', function($rootScope, $scope) {
+    var conversionRate = 0.3529;
+    $rootScope.unit = "Cum";
+    $rootScope.unitName = function () {
+        console.log("unit: " + $rootScope.unit);
+        if($rootScope.unit == "Cum") {
+            $rootScope.unit = "Brass";
+        }else{
+            $rootScope.unit = "Cum";
+        }
+    }
+    var ratio = null;
+    try {
+        ratio = $rootScope.ratio();
+        $rootScope.ratioData = ratio;
+    } catch (err) {
+        $rootScope.ratioData = {cement: 1,
+                                cs: 0,
+                                rs: 0,
+                                aggregate: 0
+                               };
+    }
 
-})
+    $rootScope.calculateGrade = function() {
+        var cs = parseFloat($rootScope.ratioData.cs);
+        var rs = parseFloat($rootScope.ratioData.rs);
+        var aggregate = parseFloat($rootScope.ratioData.aggregate);
+        var grade = 90/(cs+rs+aggregate)
+        if(grade == Infinity){
+            return 100;
+        }
+        return grade.toFixed(0);
+    };
 
-.controller('FindCostCtrl', function($scope, Rates) {
-  $scope.friends = Friends.all();
-})
+    $rootScope.calculateQuantity = function() {
+        var mixRatios = $rootScope.ratioData;
+        var cs = mixRatios.cs;
+        var rs = mixRatios.rs;
+        var aggregate = mixRatios.aggregate;
+        var denominator = (1 + cs + rs + aggregate);
 
-.controller('RatesCtrl', function($scope, $stateParams) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
+        var cementQ = 1.52 * 28.8 / denominator;
+        var csQ = 1.52 * cs / denominator;
+        var rsQ = 1.52 * rs / denominator;
+        var aggregateQ = 1.52 * aggregate / denominator;
+
+        if($rootScope.unit == "Brass") {
+            csQ = csQ * conversionRate;
+            rsQ = rsQ * conversionRate;
+            aggregateQ = aggregateQ * conversionRate;
+        }
+
+        return {cement: parseFloat(cementQ.toFixed(2)),
+                cs: parseFloat(csQ.toFixed(3)),
+                rs: parseFloat(rsQ.toFixed(3)),
+                aggregate: parseFloat(aggregateQ.toFixed(3))
+               };
+    };
+}])
+
+.controller('FindCostCtrl', ['$rootScope', '$scope', function($rootScope, $scope, Rates) {
+
+}])
+
+.controller('RatesCtrl', ['$rootScope', '$scope', function($scope) {
+  $rootScope.materialRates = Rates.getMaterialRates();
+  $rootScope.saveMaterialRates = function () {
+      Rates.save($rootScope.materialRates);
+  };
+}])
 
 
 //Not Useful
--- www/js/services.js
+++ www/js/services.js
@@ -63,8 +63,6 @@ angular.module('starter.services', [])
         }
       }
       return null;
-    },
-    evalute: function(inputParams, formula) {
     }
   }
 })
@@ -72,46 +70,37 @@ angular.module('starter.services', [])
 /**
  * A simple example service that returns some data.
  */
-.factory('Friends', function() {
+.factory('Rates', function() {
   // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
-  // Some fake testing data
-  var friends = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    notes: 'Enjoys drawing things',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+  var materialRates = [{
+      id: 0,
+      name: "Cement",
+      rate: "350",
+      unit: "BAG"
   }, {
-    id: 1,
-    name: 'Max Lynx',
-    notes: 'Odd obsession with everything',
-    face: 'https://pbs.twimg.com/profile_images/479740132258361344/KaYdH9hE.jpeg'
+      id: 1,
+      name: "River Sand",
+      rate: "4500",
+      unit: "Brass"
   }, {
-    id: 2,
-    name: 'Andrew Jostlen',
-    notes: 'Wears a sweet leather Jacket. I\'m a bit jealous',
-    face: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg'
+      id: 2,
+      name: "Crushed Sand",
+      rate: "2500",
+      unit: "Brass"
   }, {
-    id: 3,
-    name: 'Adam Bradleyson',
-    notes: 'I think he needs to buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 4,
-    name: 'Perry Governor',
-    notes: 'Just the nicest guy',
-    face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
+      id: 3,
+      name: "Aggregate (Coarse)",
+      rate: "2200",
+      unit: "Brass"
   }];
-
 
   return {
     all: function() {
-      return friends;
+      return JSON.parse(window.localStorage['materialRates'] || JSON.stringify(materialRates));
     },
-    get: function(friendId) {
-      // Simple index lookup
-      return friends[friendId];
+    save: function(rates) {
+        window.localStorage['materialRates'] = JSON.stringify(rates);
     }
   }
 });
